6bec8b44b73c490a9498d6c0e8f85d8cd4c88c21
NaveenSatya <naganaveen.satya@gmail.com>
2022-04-21 22:30:33 +0530

    adding some variables to repo

--- roboshop/03-vars.sh
+++ roboshop/03-vars.sh
@@ -13,7 +13,7 @@ NO_OF_USERS=$(who |wc -l)
 echo Number of Users Logged= $NO_OF_USERS
 
 ## Date with command substitution
-DATE=$(date +%F)
+DATE=$(date + %F)
 echo Welcome, Today date is $DATE
 
 echo COURSE NAME = ${COURSE_NAME}
