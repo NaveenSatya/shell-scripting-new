6dbb89ded768c92c158bd741e0eabef52eb60319
NaveenSatya <naganaveen.satya@gmail.com>
2022-04-21 22:59:34 +0530

    using functions concepts in shell scripting

--- roboshop/06-functions.sh
+++ roboshop/06-functions.sh
@@ -5,15 +5,15 @@
 sample() {
   echo Hello , I am a Sample function
   echo Value of a = ${a}
-  #b=200
-  #echo First Argument in Function = $1
-  #local c=10 # scope of this variable is inside function
+  b=200
+  echo First Argument in Function = $1
+  local c=10 # scope of this variable is inside function
 }
 
 ## Main Program
 
 ## Call the function
 a=100
-#sample ABC
-#echo Value of b = ${b}
-#echo First Argument in Main Program = $1
+sample ABC
+echo Value of b = ${b}
+echo First Argument in Main Program = $1
