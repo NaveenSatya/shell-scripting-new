d868e450e94585780029ab3f36702cea2042a70d
NaveenSatya <naganaveen.satya@gmail.com>
2022-04-21 22:29:34 +0530

    adding some variables to repo

--- roboshop/03-vars.sh
+++ roboshop/03-vars.sh
@@ -6,7 +6,8 @@ echo A = $value1
 echo sign-in= $Login
 DATE="2021-07-14"
 echo Welcome, Today date is $DATE
-echo again back to practice on shell scripting
+echo -e "\e[31mAgain back to practice on shell scripting\e[0m"
+echo -e "\e[31mMESSAGE IN RED\e[0m"
 ## Command Subs
 NO_OF_USERS=$(who |wc -l)
 echo Number of Users Logged= $NO_OF_USERS
